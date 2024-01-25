84bdefbbfde096784293aff21e40e15073467469
Atir Khan <115460872+atir09@users.noreply.github.com>
2024-01-25 17:02:30 +0530

    Updated Cors for Frontend web

--- Backend/index.js
+++ Backend/index.js
@@ -20,6 +20,7 @@ app.use(
     cors({
       origin: [
         "http://localhost:3000",
+        "https://propftx-movie-app.vercel.app/",
       ],
       methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
       credentials: true,
